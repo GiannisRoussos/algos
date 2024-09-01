1e8c99a13a0c57f20b511b4471ce9cd603264481
GiannisRoussos <john71980@hotmail.com>
2024-09-02 01:04:02 +0300

    find the sum of two integers third try

--- src/main/java/global/banking/Algo1.java
+++ src/main/java/global/banking/Algo1.java
@@ -1,5 +1,6 @@
 package global.banking;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,44 +10,28 @@ public class Algo1 {
     public static void main(String[] args) {
         int[] nums = {11, 2, 15, 7};
         int target = 9;
+        //Call mehtod findPositions
+        int[] positions = findPositions(nums, target);
+        System.out.println(Arrays.toString(positions));
+    }
+
+    /**
+     * To have O(n) complexity we will use HashMap
+     */
+    public static int[] findPositions(int[] nums, int target) {
         int position1 = -1;
         int position2 = -1;
-
-        /**
-         * First Try
-         */
-        boolean found = false;
-        for (int i = 0; i < nums.length - 1 && !found; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (Math.addExact(nums[i], nums[j]) == target) {
-                    position1 = i;
-                    position2 = j;
-                    found = true;
-                    break;
-                }
-            }
-        }
-        System.out.println(position1);
-        System.out.println(position2);
-
-        /**
-         * To have O(n) complexity we will use HashMap
-         */
-        int position3 = -1;
-        int position4 = -1;
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             int completion = target - nums[i];
             if (map.containsKey(completion)) {
-                position3 = map.get(completion);
-                position4 = i;
-                break;  // Βρέθηκε το ζευγάρι, σταματάμε τον βρόχο
+                position1 = map.get(completion);
+                position2 = i;
+                break;
             }
             map.put(nums[i], i);
         }
-        System.out.println(position3);
-        System.out.println(position4);
-
+       return new int[]{position1, position2};
     }
 }
 
